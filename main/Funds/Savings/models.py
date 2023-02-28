e67ec272d11e7461c24daa72d8866112cbd74178
Shibin r Silva <shibinsr17@gmail.com>
2023-02-28 23:19:58 +0530

    create santha and savings table

---
+++ main/Funds/Savings/models.py
@@ -0,0 +1,17 @@
+from main import db
+
+class Savings(db.Model):
+    id=db.Column(db.BigInteger(),nullable=False,primary_key=True)
+    member_id=db.Column(db.BigInteger(),nullable=False)
+    transaction_date=db.Column(db.Date(),nullable=False)
+    year=db.Column(db.Integer())
+    month=db.Column(db.Integer())
+    week=db.Column(db.Integer())
+    initial_amount=db.Column(db.Float(precision=32,decimal_return_scale=None),nullable=False)
+    transaction_amount=db.Column(db.Float(precision=32,decimal_return_scale=None),nullable=False)
+    transaction_type=db.Column(db.Integer(),server_default='0',nullable=False)
+    final_balance=db.Column(db.Float(precision=32,decimal_return_scale=None),nullable=False)
+    created_on=db.Column(db.DateTime(),server_default=db.func.now())
+
+    def __repr__(self):
+        return f"<userId:{self.id}"
