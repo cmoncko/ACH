7f9f0e80fe3504e787287b20ca5fc3bce2bbc44e
cmoncko <shikoignatious@gmail.com>
2023-03-03 14:58:48 +0530

    santha model

---
+++ main/Funds/Santha/models.py
@@ -0,0 +1,14 @@
+from main.extenstions import db
+
+class SanthaPayments(db.Model):
+    id=db.Column(db.BigInteger(),nullable=False,primary_key=True)
+    member_id=db.Column(db.BigInteger(),db.ForeignKey('member_profile.id'),nullable=False)
+    santha_for_year=db.Column(db.Integer(),nullable=False)
+    santha_amount=db.Column(db.Float(precision=32,decimal_return_scale=None),nullable=False)
+    received_amount=db.Column(db.Float(precision=32,decimal_return_scale=None))
+    received_date=db.Column(db.Date())
+    created_on=db.Column(db.DateTime(),server_default=db.func.now())
+
+
+    def __repr__(self):
+        return f"<userId:{self.id}"
